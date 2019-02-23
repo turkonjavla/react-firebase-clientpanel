4c0b9078eeec5cbf7a9d392993dfce4823ef9942
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-02-23 16:47:04 +0100

    Add Dashboard component

---
+++ src/components/clients/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-10">
+
+        </div>
+        <div className="col-md-2">
+          
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard;
